58d405c56747bea152e3357f8d8868645a7c88ba
rexfordkode <rexford.koomson@amalitech.com>
2021-08-14 02:00:24 +0100

    Init commit

---
+++ reverse Array.js
@@ -0,0 +1,7 @@
+const reverseCipher = (sentence) => {
+  let words = sentence.split(' ');
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i].split('').reverse().join('');
+  }
+   return words.join(' ');
+};
